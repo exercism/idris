ca7c9dfaadfcc94fbdd0cfc579b89cb1b528f7be
Eric Willigers <keiravillekode@gmail.com>
2024-07-19 17:36:18 +1000

    refactor bin/create-exercise.sh
    We pass author and difficulty to configlet create
    as command line arguments.
    
    We specify the module in src/ and example/

--- bin/create-exercise.sh
+++ bin/create-exercise.sh
@@ -22,8 +22,15 @@ while [[ ${pascal} =~ (.*)-(.*) ]]; do
     pascal=${BASH_REMATCH[1]}${BASH_REMATCH[2]^}
 done
 
+if [[ -z $author ]]; then
+    echo
+    read -rp "What's your github username? " author
+fi
+
+read -p "What's the difficulty for ${slug}? " difficulty
+
 bin/fetch-configlet
-bin/configlet create --practice-exercise "${slug}"
+bin/configlet create --practice-exercise "${slug}" --author "${author}" --difficulty "${difficulty}"
 
 sed -e "s#hello-world#${slug}#g" exercises/practice/hello-world/pack.toml > exercises/practice/${slug}/pack.toml
 
@@ -31,28 +38,7 @@ sed -e "s#hello-world#${slug}#g" exercises/practice/hello-world/test/test.ipkg >
 
 sed -e "s#hello-world#${slug}#g" exercises/practice/hello-world/hello-world.ipkg | sed -e "s#HelloWorld#${pascal}#g" > exercises/practice/${slug}/${slug}.ipkg
 
-
-if [[ -z $author ]]; then
+{
+    echo "module ${pascal}"
     echo
-    read -rp "What's your github username? " author
-fi
-
-conf="exercises/practice/${slug}/.meta/config.json"
-jq  --arg author "${author}" \
-    '.authors = [$author]' "${conf}" > "${conf}.tmp" \
-&& mv "${conf}.tmp" "${conf}"
-
-
-read -p "What's the difficulty for ${slug}? "
-if [[ -n ${REPLY} && ${REPLY} == +([0-9]) ]]; then
-    tmp=$(mktemp)
-    jq --argjson diff "${REPLY}" --arg slug "${slug}" '
-        .exercises.practice |= map(
-            if (.slug == $slug)
-            then .difficulty = $diff
-            else .
-            end
-        )
-    ' config.json > "${tmp}" \
-    && mv "${tmp}" config.json
-fi
+} | tee exercises/practice/${slug}/src/${pascal}.idr > exercises/practice/${slug}/example/${pascal}.idr
