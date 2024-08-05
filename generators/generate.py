e451f6dcd55f2ba0619ddf460477932083565652
Eric Willigers <keiravillekode@gmail.com>
2024-08-06 06:45:49 +1000

    Use locally cached problem spec data
    From forum suggestions
    http://forum.exercism.org/t/adding-the-roc-language-to-exercism/12360/16
    http://forum.exercism.org/t/scala-3-support/12117/5

--- generators/generate.py
+++ generators/generate.py
@@ -3,9 +3,9 @@
 import argparse
 import importlib
 import json
+import subprocess
 import sys
 import tomllib
-import urllib.request
 
 PREFIX = """module Main
 
@@ -25,10 +25,13 @@ main = do
      else exitFailure
 """
 
-def download_canonical_data(exercise):
-    url = f"https://raw.githubusercontent.com/exercism/problem-specifications/master/exercises/{exercise}/canonical-data.json"
-    with urllib.request.urlopen(url) as f:
-        return json.loads(f.read().decode())
+def read_canonical_data(exercise):
+    args = ['bin/configlet', 'info', '-o', '-v', 'd']
+    info = subprocess.run(args, capture_output=True, check=True, text=True).stdout
+    cache_dir = info.split("\n")[0].replace("Using cached 'problem-specifications' dir: ", "")
+    path = f"{cache_dir}/exercises/{exercise}/canonical-data.json"
+    with open(path, "r") as f:
+        return json.loads(f.read())
 
 def flatten_cases(canonical_data):
     result = []
@@ -77,7 +80,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("exercise")
     args = parser.parse_args()
-    cases = flatten_cases(download_canonical_data(args.exercise))
+    cases = flatten_cases(read_canonical_data(args.exercise))
     cases = filter_cases(args.exercise, cases)
 
     mod = importlib.import_module("exercises." + args.exercise.replace("-", "_"))
