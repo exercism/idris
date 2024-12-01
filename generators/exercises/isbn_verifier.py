3c4c32f63c6ca47d5df3532c29a03aa3b9ddf773
keiravillekode <keiravillekode@gmail.com>
2024-12-01 18:12:46 +1100

    Add isbn-verifier (#185)

---
+++ generators/exercises/isbn_verifier.py
@@ -0,0 +1,11 @@
+
+def generate_test(case):
+    property = "isValid"
+    isbn = case["input"]["isbn"]
+    expected = case["expected"]
+    if expected:
+        prefix = "assert $      "
+    else:
+        prefix = "assert $ not $"
+
+    return f'{prefix} {property} "{isbn}"'
